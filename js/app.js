6607b9e9098636d0445b1d930a3f74fd08b0fe4b
Bayu Slamet Riyadi <bayu93mx@gmail.com>
2017-06-19 08:52:37 +0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,101 @@
+(function(e) {
+    e(window.jQuery, window, document);
+})(function($, window, document) {
+
+    var app = {
+
+        // ==============================================================================================
+        // Call your function here to become a single function
+        // * This method make your code more modular and make it easy to toggle your function
+        // * If you want to disable a function, just commented on function that you need to disable below
+        // ==============================================================================================
+
+        init: function($) {
+            app.addBlockTable();
+            app.menuToggle();
+            app.owlSlider();
+        },
+
+        // ======================================================================
+        // Your function here
+        // * Don't forget to use proper function name to describes your function
+        // ======================================================================
+        menuToggle: function() {
+            $('.btn-toggle').on('click', function() {
+                $('body').toggleClass('toggle-sidebar');
+            });
+            // toggle menu
+            $('.table-toggle').click(function() {
+                $(this).parent().find($('.table-menu-link')).toggleClass('show');
+            });
+            if (jQuery(window).width() <= 992) {
+                jQuery('.menu-toggle').appendTo('header');
+            }
+        },
+        addBlockTable: function() {
+            jQuery('#table-home td:last-child').append('<div class="table-menu">' +
+                '<a href="#0" class="table-toggle"><i class="icon-options-vertical"></i></a>' +
+                '<div class="table-menu-link">' +
+                '<span class="">' +
+                '<a href="#">' +
+                '<i class="icon-user"></i>' +
+                '</a>' +
+                '</span>' +
+                '<span class="">' +
+                '<a href="#">' +
+                '<i class="icon-reload"></i>' +
+                '</a>' +
+                '</span>' +
+                '<span class="">' +
+                '<a href="#">' +
+                '<i class="icon-list"></i>' +
+                '</a>' +
+                '</span>' +
+                '<span class="">' +
+                '<a href="#">' +
+                '<i class="icon-trash"></i>' +
+                '</a>' +
+                '</span>' +
+                '</div>' +
+                '</div>'
+            );
+            var $table = $('#table-home'),
+                $ok = $('#ok');
+            $(function() {
+                $ok.click(function() {
+                    $table.bootstrapTable('refresh');
+                });
+            });
+
+            function queryParams() {
+                var params = {};
+                $('#toolbar').find('input[name]').each(function() {
+                    params[$(this).attr('name')] = $(this).val();
+                });
+                return params;
+            }
+
+            function responseHandler(res) {
+                return res.rows;
+            }
+
+
+        },
+        owlSlider: function() {
+            $(document).ready(function() {
+                $(".owl-carousel").owlCarousel();
+            });
+        },
+
+    };
+
+    // This code will initialize your whole function in this JS file
+    $(function() {
+        app.init($);
+    });
+
+    $(window).resize(function() {
+        // Insert your JS function here that need to triggered when window resize
+        // app.scrollToFixed();
+    });
+});
